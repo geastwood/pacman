ff5eb77207ce563daa8685737a255751412665a9
Fei Liu <liu.feiwood@gmail.com>
2014-07-22 23:40:17 +0200

    tests on move methods

--- src/Pacman.js
+++ src/Pacman.js
@@ -1,5 +1,13 @@
 define(function() {
     var Pacman = function() {};
-    Pacman.prototype.move = function(x, y) {};
+    Pacman.prototype.move = function(x, y) {
+
+        /* jshint eqnull: true */
+        if (x == null || y == null) {
+            throw 'x or y is invalid input';
+        }
+        this.x = x;
+        this.y = y;
+    };
     return Pacman;
 });
--- test/Pacman.js
+++ test/Pacman.js
@@ -5,12 +5,24 @@ define(function(require) {
         var Pacman  = require('Pacman');
         var p = new Pacman();
 
-        it('should be a constructor function', function() {
-            expect(typeof Pacman).toBe('function');
+        describe('basic', function() {
+            it('should be a constructor function', function() {
+                expect(typeof Pacman).toBe('function');
+            });
         });
-        it('should have a move method', function() {
-            expect(p.move).toBeDefined();
-            expect(typeof p.move).toBe('function');
+        describe('move', function() {
+            it('should have a move method', function() {
+                expect(p.move).toBeDefined();
+                expect(typeof p.move).toBe('function');
+            });
+            it('should receive x and y as parameters', function() {
+                expect(p.move).toThrow();
+                expect(function() { return p.move(2, null); }).toThrow();
+                expect(function() { return p.move(222, undefined); }).toThrow();
+                expect(function() { return p.move(null, 33); }).toThrow();
+                expect(function() { return p.move(undefined, 22); }).toThrow();
+                expect(function() { return p.move(33, 22); }).not.toThrow();
+            });
         });
     });
 });
