dc64f0b087f7c515e83a669eb6720de3daa6ccd1
Fei Liu <liu.feiwood@gmail.com>
2014-07-23 17:47:51 +0200

    more on move

--- src/Pacman.js
+++ src/Pacman.js
@@ -1,13 +1,37 @@
 define(function() {
-    var Pacman = function() {};
+    var Pacman = function(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+        this.state = 'inactive';
+    };
+    Pacman.prototype.setState = function(newState) {
+        this.state = newState;
+    };
     Pacman.prototype.move = function(x, y) {
 
         /* jshint eqnull: true */
         if (x == null || y == null) {
             throw 'x or y is invalid input';
         }
+
+        if (this.state === 'inactive') {
+            return;
+        }
+
         this.x = x;
         this.y = y;
+    };
+    Pacman.prototype.moveUp = function() {
+        return this.move(this.x, this.y + 1);
+    };
+    Pacman.prototype.moveDown = function() {
+        return this.move(this.x, this.y - 1);
+    };
+    Pacman.prototype.moveLeft = function() {
+        return this.move(this.x - 1, this.y);
+    };
+    Pacman.prototype.moveRight = function() {
+        return this.move(this.x + 1, this.y);
     };
     return Pacman;
 });
--- test/Pacman.js
+++ test/Pacman.js
@@ -23,6 +23,71 @@ define(function(require) {
                 expect(function() { return p.move(undefined, 22); }).toThrow();
                 expect(function() { return p.move(33, 22); }).not.toThrow();
             });
+            it('should have "moveUp", "moveDown", "moveLeft", "moveRight"', function() {
+                expect(p.moveUp).toBeDefined();
+                expect(p.moveDown).toBeDefined();
+                expect(p.moveLeft).toBeDefined();
+                expect(p.moveRight).toBeDefined();
+            });
+        });
+        describe('move correctly', function() {
+            var p;
+            beforeEach(function() {
+                p = new Pacman(20, 20);
+                p.setState('active');
+            });
+            it('up correctly', function() {
+                p.moveUp();
+                expect(p.x).toBe(20);
+                expect(p.y).toBe(21);
+            });
+            it('down correctly', function() {
+                p.moveDown();
+                expect(p.x).toBe(20);
+                expect(p.y).toBe(19);
+                p.moveDown();
+                expect(p.x).toBe(20);
+                expect(p.y).toBe(18);
+            });
+            it('left correctly', function() {
+                p.moveLeft();
+                p.moveDown();
+                expect(p.x).toBe(19);
+                expect(p.y).toBe(19);
+            });
+            it('right correctly', function() {
+                p.moveLeft();
+                p.moveRight();
+                p.moveDown();
+                expect(p.x).toBe(20);
+                expect(p.y).toBe(19);
+            });
+        });
+        describe('move with state', function() {
+            var p;
+            beforeEach(function() {
+                p = new Pacman(20, 20);
+            });
+            it('should not move without being alive', function() {
+                p.moveUp();
+                p.moveLeft();
+                expect(p.x).toBe(20);
+                expect(p.y).toBe(20);
+            });
+        });
+        describe('api for setting Status', function() {
+            var p;
+            beforeEach(function() {
+                p = new Pacman(20, 20);
+            });
+            it('default to inactive', function() {
+                expect(p.state).toBe('inactive');
+            });
+            it('possible to set to "active"', function() {
+                p.setState('active');
+                expect(p.state).toBe('active');
+            });
         });
     });
+
 });
